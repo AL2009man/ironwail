5808d55d031bbd251df23f8f8201c99ac0d7b655
AL2009man <67606569+AL2009man@users.noreply.github.com>
2025-04-30 20:29:58 -0400

    overhauled gyrospace scaling

--- Quake/in_sdl.c
+++ Quake/in_sdl.c
@@ -1750,36 +1750,39 @@ void IN_SendKeyEvents (void)
 						event.csensor.data[1] - gyro_calibration_y.value,
 						event.csensor.data[0] - gyro_calibration_x.value,
 						event.csensor.data[2] - gyro_calibration_z.value,
-						1.0f, 1.0f, 1.0f, 0.0f
+						0.0f
 					);
+
 					gyro_yaw = localGyro.x;
 					gyro_pitch = localGyro.y;
 					gyro_roll = -localGyro.z;
 				}
 				break;
+
 				case 3: // Player Space
 				{
 					Vector3 playerGyro = TransformToPlayerSpace (
 						event.csensor.data[1] - gyro_calibration_y.value,
 						event.csensor.data[0] - gyro_calibration_x.value,
 						event.csensor.data[2] - gyro_calibration_z.value,
-						GetGravityVector (),
-						1.0f, 1.0f, 1.0f
+						GetGravityVector ()
 					);
+
 					gyro_yaw = playerGyro.x;
 					gyro_pitch = playerGyro.y;
 					gyro_roll = -playerGyro.z;
 				}
 				break;
+
 				case 4: // World Space
 				{
 					Vector3 worldGyro = TransformToWorldSpace (
 						event.csensor.data[1] - gyro_calibration_y.value,
 						event.csensor.data[0] - gyro_calibration_x.value,
 						event.csensor.data[2] - gyro_calibration_z.value,
-						GetGravityVector (),
-						1.0f, 1.0f, 1.0f
+						GetGravityVector ()
 					);
+
 					gyro_yaw = worldGyro.x;
 					gyro_pitch = worldGyro.y;
 					gyro_roll = -worldGyro.z;
@@ -1793,6 +1796,7 @@ void IN_SendKeyEvents (void)
 					break;
 				}
 
+				// Preserve direct pitch calibration
 				gyro_pitch = event.csensor.data[0] - gyro_calibration_x.value;
 
 				// Save unfiltered magnitude for UI display
